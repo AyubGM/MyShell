705afde6f3fe8a1733a5917f7f0b68084fff59f8
AyubGM <ayubgmdev@gmail.com>
2026-09-15 23:09:03 +0200

    Tokenizer

--- MyShell/src/MyShell.cpp
+++ MyShell/src/MyShell.cpp
@@ -1,10 +1,79 @@
-// MyShell.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <print>
+#include <filesystem>
+#include <format>
+#define NOMINMAX
+#include <windows.h>
 
-int main()
+void PrintTokens(const std::vector<std::wstring>& tokens)
 {
-    std::cout << "Hello World!\n";
+    for (size_t i{}; i < tokens.size(); i++)
+    {
+      std::wcout << std::format(L"Token{}: {}", i, tokens[i]) << L'\n';
+    }
 }
+
+std::vector<std::wstring> TokenizeCommand(const std::wstring& line)
+{
+    std::vector<std::wstring> tokens;
+    std::wstring currentToken;
+
+    bool inQuotes = false;
+
+    for (wchar_t ch : line)
+    {
+        if (ch == L'"')
+        {
+            inQuotes = !inQuotes; // Toggle state when encountering quotes
+        }
+        else if (ch == L' ' && !inQuotes)
+        {
+            if (!currentToken.empty())
+            {
+                tokens.push_back(currentToken);
+                currentToken.clear();
+            }
+        }
+        else
+        {
+            currentToken += ch;
+        }
+    }
+
+    if (!currentToken.empty())
+    {
+        tokens.push_back(currentToken);
+    }
+
+    return tokens;
+}
+
+int main(int argc, char* argv[])
+{
+    while (true)
+    {
+        std::print("{}>", std::filesystem::current_path().string());
+
+        std::wstring line;
+        if (!std::getline(std::wcin, line) || line.empty())
+        {
+            std::println("empty input");
+            continue;
+        }
+
+		std::vector<std::wstring> args = TokenizeCommand(line);
+
+		if (args.empty())
+		{
+			std::println("empty input");
+			continue;
+		}
+
+        PrintTokens(args);
+
+    }
+    
+
+} 
 
